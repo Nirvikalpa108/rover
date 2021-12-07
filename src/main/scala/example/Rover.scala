f96b02c129989d930dfe1a44920c37651b4a7785
Amina Adewusi <amina.adewusi@guardian.co.uk>
2021-12-07 10:19:38 +0000

    all tests passing

--- src/main/scala/example/Rover.scala
+++ src/main/scala/example/Rover.scala
@@ -1,22 +1,66 @@
 package example
 
 object Rover {
-
-  def moveRover(grid: Grid, coord: Coord, directionInput: String, commandInput: String): Either[String, RoverState] = {
+  def rove(gridInput: Grid, coordInput: Coord, directionInput: String, commandInput: String): Either[String, RoverState] = {
     for {
-      g <- validateGrid(grid)
-      roverCoord <- validatedCoords(g, coord)
-      roverDirection <- validateDirection(directionInput)
-      command <- validatedCommand(commandInput)
-      roverState = RoverState(roverCoord, roverDirection)
-    } yield move(roverState, command)
+      grid <- validateGrid(gridInput)
+      coord <- validateCoords(grid, coordInput)
+      direction <- validateDirection(directionInput)
+      command <- validateCommand(commandInput)
+      state = RoverState(coord, direction)
+    } yield move(command, state, grid)
   }
 
-  def move(roverState: RoverState, command: Command): RoverState = ???
-  def validateGrid(grid: Grid): Either[String, Grid] = ???
-  def validatedCoords(grid: Grid, coord: Coord): Either[String, Coord] = ???
-  def validateDirection(direction: String): Either[String, Direction] = ???
-  def validatedCommand(command: String): Either[String, Command] = ???
+  private[example] def move(command: Command, r: RoverState, grid: Grid): RoverState = command match {
+    case Forward => RoverState(moveForward(r, grid), r.direction)
+    case RotateClockwise => RoverState(r.coord, rotateClockwise(r.direction))
+    case RotateAntiClockwise => RoverState(r.coord, rotateAntiClockwise(r.direction))
+  }
+
+  /**
+   * Given the rover's direction and grid boundaries, determine it's new coordinates after moving forward.
+   */
+  private def moveForward(rover: RoverState, g: Grid): Coord = {
+    val c = rover.coord
+    rover.direction match {
+      case North => if (c.y == g.y) Coord(c.x, 1) else Coord(c.x, c.y + 1)
+      case East => if (c.x == g.x) Coord(1, c.y) else Coord(c.x + 1, c.y)
+      case South => if (c.y == 1) Coord(c.x, g.y) else Coord(c.x, c.y - 1)
+      case West => if (c.x == 1) Coord(g.x, c.y) else Coord(c.x - 1, c.y)
+    }
+  }
+  private def rotateClockwise(d: Direction): Direction = d match {
+    case North => East
+    case East => South
+    case South => West
+    case West => North
+  }
+  private def rotateAntiClockwise(d: Direction): Direction = d match {
+    case North => West
+    case East => North
+    case South => East
+    case West => South
+  }
+  private[example] def validateGrid(grid: Grid): Either[String, Grid] = if (grid.x <= 0 || grid.y <= 0) Left(invalidGrid) else Right(grid)
+  private[example] def validateCoords(grid: Grid, coord: Coord): Either[String, Coord] = if (grid.x >= coord.x && grid.y >= coord.y) Right(coord) else Left(invalidCoords)
+  private[example] def validateDirection(direction: String): Either[String, Direction] = direction match {
+    case "N" => Right(North)
+    case "E" => Right(East)
+    case "S" => Right(South)
+    case "W" => Right(West)
+    case _ => Left(invalidDirection)
+  }
+  private[example] def validateCommand(command: String): Either[String, Command] = command match {
+    case "F" => Right(Forward)
+    case "RC" => Right(RotateClockwise)
+    case "RAC" => Right(RotateAntiClockwise)
+    case _ => Left(invalidCommand)
+  }
+
+  val invalidGrid = "invalid grid"
+  val invalidCoords = "invalid coordinates"
+  val invalidDirection = "invalid direction"
+  val invalidCommand = "invalid command"
 }
 
 case class RoverState(coord: Coord, direction: Direction)
@@ -24,13 +68,14 @@ case class Grid(x: Int, y: Int)
 case class Coord(x: Int, y: Int)
 
 sealed trait Direction
-case object N extends Direction
-case object E extends Direction
-case object S extends Direction
-case object W extends Direction
+case object North extends Direction
+case object East extends Direction
+case object South extends Direction
+case object West extends Direction
 
 sealed trait Command
 case object Forward extends Command
 case object RotateClockwise extends Command
 case object RotateAntiClockwise extends Command
+
 
--- src/test/scala/example/RoverSpec.scala
+++ src/test/scala/example/RoverSpec.scala
@@ -1,10 +1,84 @@
 package example
 
+import example.Rover._
+import org.scalatest.EitherValues
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
-class RoverSpec extends AnyFreeSpec with Matchers {
-  "" - {
-    "" ignore {}
+class RoverSpec extends AnyFreeSpec with Matchers with EitherValues {
+  "validateGrid" - {
+    "given a 4x4 grid, return a Right" in {
+      validateGrid(Grid(4,4)) should matchPattern { case Right(Grid(4,4)) => }
+    }
+    "given a 0x0 grid, return a Left" in {
+      validateGrid(Grid(0,0)) should matchPattern { case Left(Rover.invalidGrid) => }
+    }
+    "given a -1x4 grid, return a Left" in {
+      validateGrid(Grid(-1,4)) should matchPattern { case Left(Rover.invalidGrid) => }
+    }
+  }
+  "validateCoords" - {
+    "given co-ordinates present on a grid, return a Right" in {
+      validateCoords(Grid(2, 2), Coord(1, 1)) should matchPattern { case Right(Coord(1,1)) => }
+    }
+    "given co-ordinates that are not present on a grid, return a Left" in {
+      validateCoords(Grid(2, 2), Coord(3, 1)) should matchPattern { case Left(Rover.invalidCoords) => }
+    }
+  }
+  "validateDirection" - {
+    "given N, return a Right of North" in {
+      validateDirection("N") should matchPattern { case Right(North) => }
+    }
+    "given E, return a Right of East" in {
+      validateDirection("E") should matchPattern { case Right(East) => }
+    }
+    "given s, return a Left" in {
+      validateDirection("s") should matchPattern { case Left(Rover.invalidDirection) => }
+    }
+    "given Z return a Left" in {
+      validateDirection("Z") should matchPattern { case Left(Rover.invalidDirection) => }
+    }
+  }
+  "validateCommand" - {
+    "given F, return a Right of Forward" in {
+      validateCommand("F") should matchPattern { case Right(Forward) => }
+    }
+    "given RC, return a Right of Rotate Clockwise" in {
+      validateCommand("RC") should matchPattern { case Right(RotateClockwise) => }
+    }
+    "given RAC, return a Right of Rotate AntiClockwise" in {
+      validateCommand("RAC") should matchPattern { case Right(RotateAntiClockwise) => }
+    }
+    "given f, return a Left" in {
+      validateCommand("f") should matchPattern { case Left(Rover.invalidCommand) => }
+    }
+    "given Z, return a Left" in {
+      validateCommand("Z") should matchPattern { case Left(Rover.invalidCommand) => }
+    }
+  }
+  "move" - {
+    val grid = Grid(4,4)
+    val roverState = RoverState(Coord(1,1), North)
+    "given the Forward command, go forward one unit" in {
+      move(Forward, roverState, grid) shouldEqual RoverState(Coord(1,2), North)
+    }
+    "given the RotateClockwise command, turn correctly" in {
+      move(RotateClockwise, roverState, grid) shouldEqual RoverState(Coord(1,1), East)
+    }
+    "given the RotateAntiClockwise command, turn correctly" in {
+      move(RotateAntiClockwise, roverState, grid) shouldEqual RoverState(Coord(1,1), West)
+    }
+    "given a Forward command that leads the rover off the grid at the top, return the correct coordinates" in {
+      move(Forward, RoverState(Coord(1,4), North), grid) shouldEqual RoverState(Coord(1,1), North)
+    }
+    "given a Forward command that leads the rover off the grid at the bottom, return the correct coordinates" in {
+      move(Forward, RoverState(Coord(4,2), East), grid) shouldEqual RoverState(Coord(1,2), East)
+    }
+    "given a Forward command that leads the rover off the grid's bottom left side, return the correct coordinates" in {
+      move(Forward, RoverState(Coord(1,1), West), grid) shouldEqual RoverState(Coord(4,1), West)
+    }
+    "given a Forward command that leads the rover off the grid's top right side, return the correct coordinates" in {
+      move(Forward, RoverState(Coord(4,4), East), grid) shouldEqual RoverState(Coord(1,4), East)
+    }
   }
 }
